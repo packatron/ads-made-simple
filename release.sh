1ea105292dda0683edc8a712aa6e5c9fcbab683a
Francesco Bianco <info.francescobianco@gmail.com>
2019-07-02 02:47:01 +0200

    publish

--- release.sh
+++ release.sh
@@ -2,15 +2,19 @@
 set -e
 WORKDIR=$(echo $PWD)
 
-mkdir -p build
-rm -fr build
-mkdir -p build
+mkdir -p ${WORKDIR}/build
+rm -fr ${WORKDIR}/build
+mkdir -p ${WORKDIR}/build
 
-cd build
+cd ${WORKDIR}/build
 git clone https://github.com/wp-quality/ads-made-simple
 
-cd ads-made-simple
+cd ${WORKDIR}/build/ads-made-simple
 composer install --no-dev --prefer-dist
+
+cd ${WORKDIR}/build
+zip -r ../dist/ads-made-simple.zip ads-made-simple \
+    -x "ads-made-simple/.git/*" "ads-made-simple/.idea/*"
 
 #cd ${WORKDIR}
 #rm -fr build
