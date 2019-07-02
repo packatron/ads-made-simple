fa73751d7d5e9e4deb0ef90f50c999f660f6464b
Francesco Bianco <info.francescobianco@gmail.com>
2019-07-02 02:56:51 +0200

    preparing publish

--- publish.sh
+++ publish.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 git add .
-git commit -am "publish"
+git commit -am "preparing publish"
 git push
 
 sleep 3
 
 docker-compose build release
 docker-compose run --rm release
+
+git add .
+git commit -am "publish release"
+git push
--- release.sh
+++ release.sh
@@ -17,7 +17,11 @@ composer install --no-dev --prefer-dist
 
 cd ${WORKDIR}/build
 zip -r ../dist/ads-made-simple-${VERSION}.zip ads-made-simple \
-    -x "ads-made-simple/.git/*" "ads-made-simple/.idea/*"
+    -x "ads-made-simple/.git/*" \
+       "ads-made-simple/.idea/*" \
+       "ads-made-simple/.*" \
+       "ads-made-simple/wp-content/*" \
+       "ads-made-simple/*.{yml,sh}" \
 
 #cd ${WORKDIR}
 #rm -fr build
